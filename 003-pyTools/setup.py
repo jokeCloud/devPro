9af4daec610eda24c2f7e48d3f7f1e00b35a2c2f
jokecloud <medalhaemerson@gmail.com>
2022-06-01 06:02:17 -0300

    setup.py

--- 003-pyTools/setup.py
+++ 003-pyTools/setup.py
@@ -63,7 +63,7 @@ def find_package_data(
                         or fn.lower() == pattern.lower()):
                         bad_name = True
                         if show_ignored:
-                            print >> sys.stderr, (
+                            print(file=sys.stderr), (
                                 "Directory %s ignored by pattern %s"
                                 % (fn, pattern))
                         break
@@ -86,7 +86,7 @@ def find_package_data(
                         or fn.lower() == pattern.lower()):
                         bad_name = True
                         if show_ignored:
-                            print >> sys.stderr, (
+                            print(file=sys.stderr), (
                                 "File %s ignored by pattern %s"
                                 % (fn, pattern))
                         break
